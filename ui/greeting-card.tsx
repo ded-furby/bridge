565abc5e6abb37c6a0a424953c15bb51d233935c
ded-furby <arjunavtani4@gmail.com>
2025-05-25 23:07:39 +1000

    Add files via upload

---
+++ ui/greeting-card.tsx
@@ -0,0 +1,47 @@
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+
+function GCard({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="card"
+      className={cn(
+        "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-10 shadow-sm",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+
+function GCardTitle({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="card-title"
+      className={cn("leading-none font-semibold", className)}
+      {...props}
+    />
+  )
+}
+
+
+
+function GCardContent({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="card-content"
+      className={cn("px-6", className)}
+      {...props}
+    />
+  )
+}
+
+
+
+export {
+  GCard,
+  GCardTitle,
+  GCardContent,
+}
